c5c937f00b2ddc2c39439095561ed25f12ce1c4d
Yavuz Bektas <yavuzbektas@gmail.com>
2021-06-04 16:22:00 +0300

    odev2 javascripts

--- js_egitim/calisma/js.js
+++ js_egitim/calisma/js.js
@@ -65,3 +65,12 @@ function getOptions() {
   }
   alert(txt1);
 }
+
+    //id=fname olan nesne çağrılarak keyboard değişkenine atandı
+    const keyboard =document.querySelector('#fname');
+    //keyboard isimli değişkene "cut" etkinliği atandı ve fonksiyon ismi parametre olarak atandı
+    keyboard.addEventListener("cut",cuttingFunction);
+    //cuttingFunction isimli fonksiyon ile form inputuna yazılan girdileri klavye ctrl+c ile kesildiğinde etkinlik çalışmaktadır
+    function cuttingFunction(event){
+    console.log("etkinlik tipi: " , event.type)
+    } 
---
+++ js_egitim/odev2/assets/js/script.js
@@ -0,0 +1,95 @@
+
+function spanList(){
+let spanElements = document.querySelectorAll("li>span")
+
+for (let index = 0; index < spanElements.length; index++) {
+    const element = spanElements[index];
+    element.addEventListener("click", function() 
+{this.parentElement.style.display ="none"
+
+})
+}
+}
+
+function newElement() {
+    var node = document.createElement("LI");  
+    
+    var inputnode = document.querySelector("#task");         
+    
+    let textnode=inputnode.value.trim()
+    if (textnode==""||textnode==null ){
+        showToast("error");
+    }
+    else {
+    node.innerText=textnode; 
+    var child = document.createElement("SPAN")
+    child.innerText="x"
+    child.classList.add("close");
+    child.addEventListener("click", function() {
+        this.parentElement.style.display ="none";
+        console.log("tıklandı");
+      });
+    node.appendChild(child)  ;
+    
+    let listNode = document.getElementById("list").appendChild(node);  
+    doneElement();
+    showToast("success");  
+    inputnode.value="";
+    }
+}
+function showToast(type) {
+    $(".toast."+type).toast('show')
+}
+
+
+function deleteElement(element) {
+    
+    var nodename = document.querySelector(element);         
+    nodename.remove();
+    console.log(nodename);
+    
+    }
+    
+function doneElement(){
+    let listElements = document.querySelectorAll("li");
+    
+    for (let index = 0; index < listElements.length; index++) {
+        var element = listElements[index];
+        element.addEventListener("click", function name() {
+            if (this.classList.contains("checked")) {
+                this.classList.remove("checked");}
+            else{
+                this.classList.add("checked");
+            }
+        })
+    }
+
+}
+function localGet() {
+  
+
+  let itemsArray = localStorage.getItem("items")
+    ? JSON.parse(localStorage.getItem("items"))
+    : [];
+
+ 
+  const data = JSON.parse(localStorage.getItem("items"));
+  
+  
+}
+function localSave() {
+    let listElements = document.querySelectorAll("li");
+    console.log(listElements.forEach(element => 
+        element.textContent))
+    
+     
+}
+function liMaker(text) {
+    const li = document.createElement("li");
+    li.textContent = text;
+    ul.appendChild(li);
+  }
+spanList();
+doneElement();
+localSave();
+localGet();
